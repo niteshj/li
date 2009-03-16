ae521b2f0f4db59b87fb1f7f6378ec9741d94392
sdutta <sdutta@illusion.(none)>
2009-03-16 15:44:44 +0530

    modified:   Main.hs
    Deleting the main method as it is not required currently

--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,3 @@ import Parser.SParser
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Prim
 import Text.ParserCombinators.Parsec.Pos
-
-
-newParser = do openParenParser 
-               str <- syntacticKeywordParser 
-               closeParenParser
-               return str
-
-main = do
-  s <- getContents
-  let tokens = alexScanTokens s
-  print $ parse newParser "" tokens
