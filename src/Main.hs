3fffe56927be44dec205c94aff7d05e281f55aa3
Rohit <akrohit@iitk.ac.in>
2009-04-10 04:21:50 +0530

    Removing Boilerplate code from Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -19,7 +19,8 @@ main = do args <- getArgs
             0           -> runErrorT (evalStateT repl initialCtx)
             1           -> do program <- readFile (args !! 0)
                               runErrorT (evalStateT (evalString program) initialCtx)
---            otherwise   -> putStrLn $ "Usage is :: li [fileName]" 
+            otherwise   -> do putStrLn $ "Usage is :: li [fileName]"
+                              return $ Right ()
   
 
 
@@ -32,24 +33,9 @@ repl = do
     Nothing  -> return ()
     Just "(quit)" -> return ()
     Just line   -> do liftIO $ addHistory line
-                      let scannedTokens = alexScanTokens line
-                      if (null scannedTokens)
-                         then return ()
-                         else do let programs = parse programParser "liParser" scannedTokens
-                                 evaledProgram <- case programs of
-                                                    Left err    -> throwError "Parse error"
-                                                    Right prgms -> mapM eval prgms
-                                 let evaledProgramOutput = map show evaledProgram
-                                 if (null evaledProgramOutput)  
-                                    then return ()
-                                    else liftIO $ putStr $ unlines $ filter (/= "") evaledProgramOutput 
-                             
-                      repl        
-
-                  `catchError` (\e -> do liftIO $ putStrLn e 
-                                         repl)
-
-
+                      evalString line
+                      repl
+                      
 
 evalString string = do let scannedTokens = alexScanTokens string
                        if (null scannedTokens)
@@ -67,33 +53,3 @@ evalString string = do let scannedTokens = alexScanTokens string
                                               
 
 
-
-
-
-{-
-repl :: StateT Context SError ()
-repl = do 
-  liftIO $ hFlush stdout
-  line <- liftIO $ readline "li > "
-  case line of
-    Nothing  -> return ()
-    Just "(quit)" -> return ()
-    Just line   -> do liftIO $ addHistory line
-                      let scannedTokens = alexScanTokens line
-                      if (null scannedTokens)
-                         then return ()
-                         else do let program = parse programParser "fail" scannedTokens
-                                 evaledProgram <- case program of
-                                                    Left err   -> throwError "Parse error"
-                                                    Right prgm -> eval prgm
-                                 let evaledProgramOutput = show evaledProgram
-                                 if (null evaledProgramOutput)  
-                                    then return ()
-                                    else liftIO $ putStrLn $ evaledProgramOutput -- evaledProgram :: Exp
-                             
-                      repl        
-
-                  `catchError` (\e -> do liftIO $ putStrLn e 
-                                         repl)
-
--}
